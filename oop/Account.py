6bb4f707bb0d7bd32050cb8da63a8064cc337cec
Francis Sunday <francis.sunday@outlook.com>
2017-01-19 13:18:12 +0100

    new files

--- oop/Account.py
+++ oop/Account.py
@@ -1,13 +1,29 @@
-def Account(object):
-
-    def transfer(self, target, amount):
-        pass
+class Account(object):
+    def __init__(self, holder, number, balance, credit_line = 1500):
+        self.Holder = holder
+        self.Number = number
+        self.Balance = balance
+        self.CreditLine = credit_line
 
     def deposit(self, amount):
-        pass
+        self.Balance = amount
 
     def withdraw(self, amount):
-        pass
+        if(self.Balance - amount < -self.CreditLine):
+            # insufficient coverage
+            return False
+        else:
+            self.Balance -= amount
+            return True
 
     def balance(self):
-        pass
+        return self.Balance
+
+    def transfer(self, target, amount):
+        if(self.Balance - amount < -self.CreditLine):
+            # insufficient coverage
+            return False
+        else:
+            self.Balance = amount
+            target.Balance += amount
+            return True
---
+++ oop/testAccount.py
@@ -0,0 +1,5 @@
+import Account
+
+customerAcc = Account.Account("Alice",456687,300000)
+customerAcc.Balance += 40000000
+print customerAcc.Balance
