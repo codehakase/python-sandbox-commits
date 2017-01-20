5a90060544c5b5d5f287166a16d9899540c950a2
Francis Sunday <francis.sunday@outlook.com>
2017-01-20 08:26:04 +0100

    runner script updated

--- oop/runner.py
+++ oop/runner.py
@@ -1,8 +1,13 @@
 from Encapsulation import Encapsulation
 
 x = Encapsulation(11,12,13)
-print x.public # can be accessed outside the class
+x.public # can be accessed outside the class
 x._protected = 40
-print x._protected # is also accesible outside the class
+x._protected # is also accesible outside the class
 
-print x.__private # will return a Traceback error, because it is a private object
+
+
+from Account import Account
+print Account.counter
+a1 = Account("Johnny Fire", 4558445, 1000000)
+print Account.counter
