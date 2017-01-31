7ecb77c932a35d51cafcbe5a16ec1214ec0b40f0
Francis Sunday <francis.sunday@outlook.com>
2017-01-31 17:24:52 +0100

    Homestudy

--- oop/AndelaHomeStudy/ShoppingCart.py
+++ oop/AndelaHomeStudy/ShoppingCart.py
@@ -1,44 +1,44 @@
 # HST2 Andela Proctor OOP Lab
-
 class ShoppingCart(object):
     def __init__(self):
         self.total = 0
         self.items = {}
 
     def add_item(self, item_name, quantity, price):
-        item_name = str(item_name)
-        if type(quantity) == int or type(price) == int:
-            cost = quantity * price
-            self.total += cost
-            self.items[item_name] = quantity
-        else:
-            quantity = int(quantity)
-            price = int(price)
-            cost = quantity * price
-            self.total += cost
-            self.items[item_name] = quantity
+        if not isinstance(item_name, basestring):
+            return False
+        elif not isinstance(quantity, int):
+            quantity = 0
+        elif not isinstance(price, int):
+            price = 0
+        cost = quantity * price
+        self.total += cost
+        self.items[item_name] = quantity
+        return True
 
     def remove_item(self, item_name, quantity, price):
-        item_name = str(item_name)
-        if type(quantity) == int or type(price) == int:
-            if item_name in self.items:
-                if not quantity < self.items[item_name]:
-                    del self.items[item_name]
-                else:
-                    self.items[item_name] -= quantity
+        if not isinstance(item_name, basestring):
+            return False
+        elif not isinstance(quantity, int):
+            quantity = 0
+        elif not isinstance(price, int):
+            price = 0
+
+        if item_name in self.items:
+            if  not quantity <= self.items[item_name]:
+                 self.items.pop(item_name, None)
+            else:
+                self.items[item_name] -= quantity
                 cost = quantity * price
                 self.total -= cost
-                
-            else:
-                pass
-        else:
-            pass
 
     def checkout(self, cash_paid):
-        cash_paid = int(cash_paid)
-        if not cash_paid > self.total:
+        if not isinstance(cash_paid, int):
+            return False
+
+        if cash_paid < self.total:
             return "Cash paid not enough"
-        elif cash_paid > self.total:
+        elif  cash_paid >= self.total:
             bal = cash_paid - self.total
             return bal
         else:
@@ -49,20 +49,12 @@ class Shop(ShoppingCart):
         ShoppingCart.__init__(self)
         self.quantity = 100
 
-    def remove_item(self, item_name = '', quantity = '', price = ''):
-        if item_name == '' or quantity == ''  or price == '':
+    def remove_item(self, item_name = None, quantity = None, price = None):
+        if not item_name or not quantity or not price:
             self.quantity -= 1
 
 cart = ShoppingCart()
 
-cart.add_item(123, '200', '100')
+cart.add_item('Mango', '20', 100)
 print cart.total
 print cart.items
-cart.remove_item('Mango', 20, 100)
-print cart.items
-print cart.checkout(10000)
-
-shop = Shop()
-print shop.quantity
-shop.remove_item()
-print shop.quantity
